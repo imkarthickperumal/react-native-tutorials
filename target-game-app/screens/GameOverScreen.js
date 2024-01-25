9f7d69926f86aa0102b332396df0f890e73a73ae
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-01-25 20:25:48 +0530

    React Native Tutorials | Target Game App setup & screens setup

---
+++ target-game-app/screens/GameOverScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const GameOverScreen = () => {
+  return (
+    <View>GameOverScreen</View>
+  )
+}
+
+export default GameOverScreen
---
+++ target-game-app/screens/GameScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const GameScreen = () => {
+  return (
+    <View>GameScreen</View>
+  )
+}
+
+export default GameScreen
