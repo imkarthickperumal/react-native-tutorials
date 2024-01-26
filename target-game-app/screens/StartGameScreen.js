1ed240b211f913f38ab3b8230338f58a3436a24f
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-01-26 20:55:21 +0530

    React Native Game App | Feedback buttons visual effects

--- target-game-app/screens/StartGameScreen.js
+++ target-game-app/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"  autoCapitalize="none" autoCorrect={false}/>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -18,7 +24,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -28,13 +34,13 @@ const styles = StyleSheet.create({
   },
   numberInput: {
     height: 50,
-    width:50,
+    width: 50,
     fontSize: 32,
     fontWeight: "bold",
     borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
     color: "#ddb52f",
     marginVertical: 8,
-    textAlign:'center',
+    textAlign: "center",
   },
 });
