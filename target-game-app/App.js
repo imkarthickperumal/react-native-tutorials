2e9bdd4b392b2551c501d3fdad7e4794a46498a7
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-01-29 20:12:48 +0530

    React Native Game App | Safeareaview working

--- target-game-app/App.js
+++ target-game-app/App.js
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import { StyleSheet, ImageBackground } from "react-native";
+import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-
   const [userNumber, setUsernumber] = useState();
 
   function pickedNumberHandler(pickedNumber) {
@@ -25,7 +24,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- target-game-app/screens/GameScreen.js
+++ target-game-app/screens/GameScreen.js
@@ -1,11 +1,22 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from "react-native";
 
 const GameScreen = () => {
   return (
-    <View>
-      <Text>GameScreen</Text>
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
     </View>
-  )
-}
+  );
+};
 
-export default GameScreen
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
