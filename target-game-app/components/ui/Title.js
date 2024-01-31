b724903eadfb48203bf18d7b87ab9a07f1054012
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-01-31 14:42:20 +0530

    React Native Game App | Creating & Displaying Random Numbers

--- target-game-app/components/ui/Title.js
+++ target-game-app/components/ui/Title.js
@@ -14,10 +14,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    color: "#ddb52f",
+    color: "white",
     textAlign: "center",
     borderWidth: 2,
-    borderColor: "#ddb52f",
+    borderColor: "white",
     padding: 12,
   },
 });
--- target-game-app/screens/GameScreen.js
+++ target-game-app/screens/GameScreen.js
@@ -1,10 +1,24 @@
 import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-const GameScreen = () => {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+const GameScreen = ({ userNumber }) => {
+  const inttialGuess = generateRandomBetween(1, 99, userNumber);
+  const [currentGuess, setCurrentGess] = useState(inttialGuess);
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
@@ -19,5 +33,6 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 24,
+    marginTop: 50,
   },
 });
