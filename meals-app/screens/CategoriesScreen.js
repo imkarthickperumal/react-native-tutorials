ba989c81383d12bdf490fe424a468709eb79eb6a
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-02-05 20:55:22 +0530

    React Native Navigation Meals App  | Displaying items in a Grid

---
+++ meals-app/screens/CategoriesScreen.js
@@ -0,0 +1,33 @@
+import { FlatList, StyleSheet } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
+
+const style = StyleSheet.create({
+    gridItem : {
+        flex:1,
+        margin:16,
+        height:150,
+        borderRadius:8,
+        elevation:4
+    }
+})
