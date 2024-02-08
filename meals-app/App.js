28258af979ebee72e2fa1fab8107db4b8d17fb63
imkarthickperumal <karthickperumal.vp@gmail.com>
2024-02-08 20:05:14 +0530

    React Native Navigation Meals App  | Working with React Navigation

--- meals-app/App.js
+++ meals-app/App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <CategoriesScreen />
+      <NavigationContainer>
+        <Stack.Screen name="MeanCategories" component={CategoriesScreen} />
+      </NavigationContainer>
     </>
   );
 }
